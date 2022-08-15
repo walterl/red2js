1ca3ea06d555594eeb80115c164a11b228249bf9
Walter Leibbrandt <git@wrl.co.za>
2022-08-15 23:41:59 +0200

    Support comment nodes too

--- src/walterl/red2js/node_types.clj
+++ src/walterl/red2js/node_types.clj
@@ -1,12 +1,23 @@
 (ns walterl.red2js.node-types
   (:require
+    [walterl.red2js.js :as js]
     [walterl.red2js.nodes :as n]
     [walterl.red2js.node-types.common]
     [walterl.red2js.node-types.external]
     [walterl.red2js.node-types.function]
     [walterl.red2js.node-types.network]
     [walterl.red2js.node-types.parser]
-    [walterl.red2js.node-types.sequence]))
+    [walterl.red2js.node-types.sequence]
+    [walterl.red2js.util :as u]))
+
+;;; Node type: comment
+
+(defmethod n/node->js "comment"
+  [{:keys [name info]} _nodes]
+  (u/join-lines
+    [name
+     (when info
+       (js/comment-lines info))]))
 
 ;;; Node type: tab
 
