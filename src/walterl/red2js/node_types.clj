4f4ed6401e9be8525cf278b1a30b90b612fede5a
Walter Leibbrandt <git@wrl.co.za>
2022-08-15 23:38:53 +0200

    Include parser and sequence node type ns's

--- src/walterl/red2js/node_types.clj
+++ src/walterl/red2js/node_types.clj
@@ -4,20 +4,12 @@
     [walterl.red2js.node-types.common]
     [walterl.red2js.node-types.external]
     [walterl.red2js.node-types.function]
-    [walterl.red2js.node-types.network]))
+    [walterl.red2js.node-types.network]
+    [walterl.red2js.node-types.parser]
+    [walterl.red2js.node-types.sequence]))
 
 ;;; Node type: tab
 
 (defmethod n/node->js "tab"
   [{:keys [label]} _nodes]
   (str "/// Tab: " label))
-
-(comment
-  (def flows walterl.red2js/flows)
-  ;; Unsupported node types:
-  (->> flows
-       (map :type)
-       (remove #{"function" "http request" "amqp-broker" "amqp-in" "amqp-out"
-                 "no-op" "switch" "inject" "debug"})
-       (frequencies))
-  ,)
